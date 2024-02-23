cda57f2be2a9193bfde43450b0dbbb0181efcbed
hgheiberger <hgheiberger@gmail.com>
2024-02-23 22:20:58 +0000

    Make drive commands publish faster

--- src/sandbox/sandbox/move_robot.py
+++ src/sandbox/sandbox/move_robot.py
@@ -11,13 +11,13 @@ class DrivePublisher(Node):
     def __init__(self):
         super().__init__('drive_publisher')
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         drive_msg = Twist()
-        drive_msg.linear.x = 0.5
-        drive_msg.angular.z = 0.0
+        drive_msg.linear.x = 1.0
+        drive_msg.angular.z = -1.0
         self.publisher_.publish(drive_msg)
         self.get_logger().info('Publishing drive command')
 
