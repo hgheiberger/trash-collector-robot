b189fde27a51c8379efa3122437f477cb372284c
Benjamin Soria <63571301+Benjamin-So@users.noreply.github.com>
2024-03-01 15:41:12 -0500

    parking controller initial version

---
+++ src/sandbox/sandbox/parking_controller.py
@@ -0,0 +1,117 @@
+import rclpy
+from rclpy.node import Node
+import math
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+
+class ParkingController(Node):
+    """
+    A controller for parking in front of a cone.
+    Listens for a relative cone location and publishes control commands.
+    Can be used in the simulator and on the real robot.
+    """
+    def __init__(self):
+        ## will receive an x,y coord
+        self.subscription = self.create_subscription(String,'/relative_cone', self.relative_cone_callback, queue_size = 10)
+        self.drive_pub = self.create_publisher(Twist, '/cmd_vel', queue_size=10)
+        self.error_pub = self.create_publisher(String, 'parking_error', queue_size=10)
+        self.data_logger = self.create_publisher(String, 'data', queue_size = 10)
+        self.subscription  # prevent unused variable warning
+
+
+        # May not exist on
+        self.declare_parameter('parking_distance', 0.15)
+        self.declare_parameter('VELOCITY', 0.75)
+        self.declare_parameter('my_double_array', [1.1, 2.2])
+
+
+        # self.parking_distance = 0.15 if self.LineFollower else 0.5 # meters; try playing with this number!
+        self.relative_x = 0
+        self.relative_y = 0
+
+        self.orientation = False
+
+    
+    def relative_cone_callback(self, msg):
+        """
+        Park car facing object given x_pos and y_pos
+        """
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+        self.relative_x = msg.x_pos
+        self.relative_y = msg.y_pos
+        L = 0.325 # Wheel base TODO Need to Change
+        eta = math.atan2(self.relative_y, self.relative_x)
+        cone_dist = math.sqrt(self.relative_x**2 + self.relative_y**2)
+        self.get_logger().info("Cone Dist: " + str(cone_dist) + " x: " + str(self.relative_x) + " y: " + str(self.relative_y))
+        setpoint = self.parking_distance
+        threshold = .3 # meters
+        
+        # look_ahead = 0.5
+        # steer_angle = math.atan(2*L*math.sin(eta)/look_ahead)
+        # steer_angle = math.atan( (L*math.sin(eta)) / ((look_ahead/2) + (look_ahead* math.cos(eta))))
+
+        look_ahead = 1 * self.VELOCITY
+        steer_angle = math.atan(2*L*math.sin(eta)/look_ahead)
+        #steer_angle = math.atan((L*math.sin(eta)) / ((look_ahead/2) + (look_ahead*math.cos(eta))))
+        drive_msg = Twist()
+
+        if self.orientation == False:
+            if cone_dist > setpoint + threshold: # Cone too far
+                # self.drive(self.VELOCITY, steer_angle)
+                drive_msg.linear.x = self.VELOCITY
+                drive_msg.angular.z = steer_angle
+                self.drive_pub.publish(drive_msg)
+
+            elif cone_dist < setpoint - threshold: # cone too close
+                self.orientation = True
+                # self.drive(-self.VELOCITY,-steer_angle)
+                drive_msg.linear.x = -self.VELOCITY
+                drive_msg.angular.z = -steer_angle
+                self.drive_pub.publish(drive_msg)
+
+
+            else: # cone within range
+                # rospy.loginfo("Cone within range ")
+                if abs(eta) > .15:
+                    self.orientation = True
+                    # self.drive(-self.VELOCITY, -steer_angle)
+                    drive_msg.linear.x = -self.VELOCITY
+                    drive_msg.angular.z = -steer_angle
+                    self.drive_pub.publish(drive_msg)
+                else:
+                    self.drive(0,0)
+                    drive_msg.linear.x = 0
+                    drive_msg.angular.z = 0
+                    self.drive_pub.publish(drive_msg)
+
+        else:
+            if (cone_dist > (setpoint - threshold) or (eta < 0.05)):
+                self.orientation = False
+
+            else:
+                # self.drive(-self.VELOCITY, -steer_angle)
+                drive_msg.linear.x = -self.VELOCITY
+                drive_msg.angular.z = -steer_angle
+                self.drive_pub.publish(drive_msg)
+
+        self.error_publisher()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    parking_controller = ParkingController()
+
+    rclpy.spin(parking_controller)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    parking_controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
